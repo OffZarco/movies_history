e172f88801ad4bb61b4955b4e6fcd1fc4cf6b607
Alexandre Cousin <82698581+OffZarco@users.noreply.github.com>
2023-01-16 23:40:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-window.onload = sortItems()
+let sortOrder = 1;
+window.onload = sortItems();
+
+var btnSort = document.getElementById("btnSort");
+var values = ["A > Z", "Z > A"];
+var currentValue = 1;
 
 function sortItems() {
     fetch("movies.json")
         .then(res => res.json())
         .then(films => {
+            films.sort((a, b) => (a.title > b.title) ? sortOrder : -sortOrder);
+            sortOrder *= -1;
             let placeholder = document.querySelector("#data-output");
             let out = "";
             for (let film of films) {
                 out += `
             <td class="td-list"> 
-            <img class="image" src='${film.img_url}'>
-            <li class="list film_title">${film.title}</li> 
+            <img class="image" src='${film.img_url}'> 
+            <li class="list">${film.title}</li> 
             <li class="list">${'Réalisateur : ' + film.real}</li> 
-            <li class="list">${'Durée : ' + film.time}</li> 
+            <li class="list">${'Durée :' + film.time}</li> 
             <li class="list">${'Année de production : ' + film.year}</li> 
             <li class="list actor">${'Acteurs : ' + film.actor}</li> 
             </td>
@@ -20,7 +27,10 @@ function sortItems() {
             }
             placeholder.innerHTML = out;
         });
+    currentValue = (currentValue + 1) % values.length;
+    btnSort.innerHTML = values[currentValue];
 }
+
 
 function searchPage() {
     let input = document.getElementById('search').value
@@ -32,7 +42,7 @@ function searchPage() {
             x[i].style.display = "none";
         }
         else {
-            x[i].style.display = "block";
+            x[i].style.display = "";
         }
     }
 }
